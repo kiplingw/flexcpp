5bb810e49b9703c3a380570f631a9ec8b494b1dc
Kip Warner <kip@thevertigo.com>
2017-08-24 17:00:40 -0700

    Fixed Generator choking on white space when checking for conflicts...

--- flexc++/generator/conflicts.cc
+++ flexc++/generator/conflicts.cc
@@ -12,16 +12,16 @@ bool Generator::conflicts() const
     if (Stat(classHeaderPath))
     {
         ret = errExisting(classHeaderPath, "class-name",
-                                "^class " + d_options.className() + "\\b")
+                                "^\\s*class\\s*" + d_options.className() + "\\b")
               or ret;
 
         if (not d_options.nameSpace().empty())
             ret = errExisting(classHeaderPath, "namespace",
-                            "^namespace " + d_options.nameSpace() + "\\b")
+                            "^\\s*namespace\\s*" + d_options.nameSpace() + "\\b")
                   or ret;
 
         ret = errExisting(classHeaderPath, Options::baseclassHeaderSpec(), 
-                    "^#include \"" + d_options.baseclassHeaderName() + '"')
+                    "^\\s*#include\\s*\"" + d_options.baseclassHeaderName() + '"')
               or ret;
     }
 
@@ -29,7 +29,7 @@ bool Generator::conflicts() const
 
     if (Stat(implementationHeader))
         ret = errExisting(implementationHeader, d_options.classHeaderSpec(), 
-                        "^#include \"" + d_options.classHeaderName() + '"')
+                        "^\\s*#include\\s*\"" + d_options.classHeaderName() + '"')
               or ret;
 
     return ret;
